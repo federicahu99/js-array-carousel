be5f5f9e3afdebb931e158f40f0b0e0f7a0146ec
feddericahu99 <federica.hu339@gmail.com>
2022-06-27 16:22:00 +0200

    added style and started script

---
+++ js/java.js
@@ -0,0 +1,12 @@
+/*
+1-inseriamo le immagini dinamicamente.
+2-rendo utilizzabile i bottoni.
+3-aggiungere un ciclo infinito al carosello.
+*/
+
+const listImg = document.querySelectorAll('#carousel img')
+
+/* Variabile per img in active */
+
+let currentWatching = 0; 
+
